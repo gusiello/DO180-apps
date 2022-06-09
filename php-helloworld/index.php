6659f58d20ff71eb0e9b56bb44da4a342520bd25
Student User <student@workstation.lab.example.com>
2022-06-09 16:45:44 -0400

    V2

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is a coming!\n";
+print "Giacomo Usiello likes studying RH courses.\n";
+echo "Thank you for reading!\n";
 ?>
